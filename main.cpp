a9df69d35e560eee3a5f9a15b6f017a31b71c052
Javier Torres <java_ta13@csu.fullerton.edu>
2015-04-29 20:43:11 -0700

    Added on to getWord1 function and completed the function.  Also, cleaned up some of the spaces and indents.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <string>
 #include <iomanip>
 using namespace std;
+
 int calculateData(string data);
 const char* hex_char_to_bin(char c);
 string hex_str_to_bin_str(const std::string& hex);
@@ -18,54 +19,65 @@ double converTime(string time);
 
 class getData
 {
-public:
-    //string address, data, cycle;
-    string sample, bgl, relTime, AbsTime, Transfer, am, address, data, size, cycle, status, iack, Fail, irq;
+	public:
+    	//string address, data, cycle;
+    	string sample, bgl, relTime, AbsTime, Transfer, am, address, data, size, cycle, status, iack, Fail, irq;
 };
 
 int main()
 {
-    vector<string> vAddress;
-    vector<string> vData;
-    vector<string> vCycle;
-    vector<string> vRelTime;
+	vector<string> vAddress;
+    	vector<string> vData;
+    	vector<string> vCycle;
+    	vector<string> vRelTime;
 
-    getData first;
-    string sample, bgl, relTime, AbsTime, Transfer, am, address, data, size, cycle, status, iack, Fail, irq;
-    ifstream inFile("test_data.log");
-    if (inFile.is_open())
-    {
-        //inFile >> address >> data >> cycle;
-        inFile >> sample >> bgl >> relTime >> AbsTime >> Transfer >> am >> address >> data >> size >> cycle >> status >> iack >> Fail >> irq;
-        first.address = address;
-        first.data = data;
-        first.cycle = cycle;
-        first.relTime = relTime;
-        vAddress.push_back(first.address);
-        vData.push_back(first.data);
-        vCycle.push_back(first.cycle);
-        vRelTime.push_back(first.relTime);
-        while (inFile.good())
-        {
-            getData next;
-            //++it;
+    	getData first;
+    	string sample, bgl, relTime, AbsTime, Transfer, am, address, data, size, cycle, status, iack, Fail, irq;
+    	ifstream inFile("test_data.log");
 
-            //inFile >> address >> data >> cycle;
-            inFile >> sample >> bgl >> relTime >> AbsTime >> Transfer >> am >> address >> data >> size >> cycle >> status >> iack >> Fail >> irq;
-            next.address = address;
-            next.data = data;
-            next.cycle = cycle;
-            next.relTime = relTime;
-            vAddress.push_back(next.address);
-            vData.push_back(next.data);
-            vCycle.push_back(next.cycle);
-            vRelTime.push_back(next.relTime);
-        }
-    }
-    else
-        cout << "Cannot open file" << endl;
+    	if (inFile.is_open())
+    	{
+        	//inFile >> address >> data >> cycle;
+        	inFile >> sample >> bgl >> relTime >> AbsTime >> Transfer >> am >> address >> data >> size >> cycle >> status >> iack >> Fail >> irq;
+        	first.address = address;
+        	first.data = data;
+        	first.cycle = cycle;
+        	first.relTime = relTime;
+        	vAddress.push_back(first.address);
+        	vData.push_back(first.data);
+        	vCycle.push_back(first.cycle);
+        	vRelTime.push_back(first.relTime);
+        	
+		while (inFile.good())
+        	{
+            		getData next;
+            		//++it;
 
-    inFile.close();
+            		//inFile >> address >> data >> cycle;
+            		inFile >> sample >> bgl >> relTime >> AbsTime >> Transfer >> am >> address >> data >> size >> cycle >> status >> iack >> Fail >> irq;
+            		next.address = address;
+            		next.data = data;
+            		next.cycle = cycle;
+            		next.relTime = relTime;
+            		vAddress.push_back(next.address);
+            		vData.push_back(next.data);
+            		vCycle.push_back(next.cycle);
+            		vRelTime.push_back(next.relTime);
+        	}
+	}
+
+	else
+        	cout << "Cannot open file" << endl;
+
+    	inFile.close();
+
+
+
+
+
+	return 0;
+}
+
 
 void getWord1(vector<string> defaultWord, vector<int> line)
 {
@@ -88,6 +100,7 @@ void getWord1(vector<string> defaultWord, vector<int> line)
 			else
 				cout << hex0 << " (unknown) " << endl;
 		}
+		
 		else if (i == 1)
 		{
 			string word1 = defaultWord[i].substr(0, 3);
@@ -102,6 +115,7 @@ void getWord1(vector<string> defaultWord, vector<int> line)
 			else
 				cout << hex1 << " (unknown) " << endl;
 		}
+
 		else if (i == 4)
 		{
 			string word4 = defaultWord[i].substr(15, 1);
@@ -122,6 +136,7 @@ void getWord1(vector<string> defaultWord, vector<int> line)
 			int hex5 = readBinary(word5);
 			cout << "Line " << line[i] << ": Word " << i << " : Cmd_ID =" << hex5 << endl;
 		}
+		
 		else if (i == 10)
 		{
 			string word10 = defaultWord[i].substr(0, 5);
@@ -141,5 +156,75 @@ void getWord1(vector<string> defaultWord, vector<int> line)
 			else
 				cout << hex15 << endl;
 		}
-}
+		
+		else if (i == 22)
+		{
+			string word22 = defaultWord[i].substr(12, 1);
+			int hex22 = readBinary(word22);
+			cout << "Line " << line[i] << ": Word " << i << ": Direction = ";
+			if (hex22 == 0)
+				cout << hex22 << " (Right) " << endl;
+			else if (hex22 == 1)
+				cout << hex22 << " (Left) " << endl;
+			else
+				cout << hex22 << endl;
+		}
 
+		else if (i == 32)
+		{
+			string word32 = defaultWord[i].substr(1, 15);
+			int hex32 = readBinary(word32);
+			cout << "Line " << line[i] << ": Word " << i << " : Num_Samples = " << hex32 << endl;
+		}
+		
+		else if (i == 37)
+		{
+			string word37 = defaultWord[i].substr(0, 1);
+			int hex37 = readBinary(word37);
+			cout << "Line " << line[i] << ": Word " << i << ": Parity = ";
+			if (hex37 == 0)
+				cout << hex37 << " (even) " << endl;
+			else if (hex37 == 1)
+				cout << hex37 << " (odd) " << endl;
+			else
+				cout << hex37 << endl;
+		}
+
+		else if (i == 38)
+		{
+			string word38 = defaultWord[i].substr(1, 1);
+			int hex38 = readBinary(word38);
+			cout << "Line " << line[i] << ": Word " << i << ": Test = ";
+			if (hex38 == 0)
+				cout << hex38 << " (disable) " << endl;
+			else if (hex38 == 1)
+				cout << hex38 << " (enable) " << endl;
+			else
+				cout << hex38 << endl;
+		}
+		
+		else if (i == 40)
+		{
+			string word40 = defaultWord[i].substr(8, 1);
+			int hex40 = readBinary(word40);
+			cout << "Line " << line[i] << ": Word " << i << ": Ctrl_Enable = ";
+			if (hex40 == 0)
+				cout << hex40 << " (disable) " << endl;
+			else if (hex40 == 1)
+				cout << hex40 << " (enable) " << endl;
+			else
+				cout << hex40 << endl;
+		}
+		
+		else if (i == 41)
+		{
+
+			string word41 = defaultWord[i].substr(1, 7);
+			int hex41 = readBinary(word41);
+			cout << "Line " << line[i] << ": Word " << i << " : Code =" << hex41 << endl;
+		}
+
+
+		i++;
+	}
+}
